f2134173fe6fd80f0408e56257ebbf2c2f77117d
MMAThijssen <mt.marijke@gmail.com>
2018-05-28 22:10:39 +0200

    Made option for horizontal plot in bar_plot()

--- graphs.py
+++ graphs.py
@@ -18,22 +18,32 @@ def pie_chart(labels, sizes):
     """
     patches, texts = plt.pie(sizes, shadow=True, startangle=90)
     plt.legend(patches, labels, loc="best")
-    plt.axis('equal')
+    plt.axis("equal")
     plt.tight_layout()
     plt.show()
 
 
-def bar_plot(labels, sizes):
-    """ Displays a bar plot.
+def bar_plot(labels, sizes, title, ylabel, vert=0):
+    """ Displays a bar plot (vert = 0: vertical, vert = 1: horizontal).
 
     labels -- list of strings, indicates labels
     sizes -- list of integers, indicates size of bars
+    ylabel -- string, label at y-axis (x-axis if vert = 1)
+    title -- string, title of plot
+    vert -- integer, indicates a vertical (0) or horizontal plot (1), default: 0
     """
     y_pos = np.arange(len(labels))
-    
-    plt.bar(y_pos, sizes, align='center', alpha=0.5)
-    plt.xticks(y_pos, labels)
-    plt.ylabel('Product group')
-    plt.title('Unique samples per product group')
+
+    if vert == 0:
+        plt.bar(y_pos, sizes, align="center", alpha=0.5)
+        plt.xticks(y_pos, labels)
+        plt.ylabel(ylabel)
+    elif vert == 1:
+        plt.barh(y_pos, sizes, align="center", alpha=0.5)
+        plt.yticks(y_pos, labels)
+        plt.xlabel(ylabel)
+    else:
+        raise ValueError("vert can have value 0 or 1.")
+    plt.title(title)
      
     plt.show()
